5275107b6780d1d551ba53832d9ca18ae7e6f87e
Svalenchuk <120367716+Svalenchuk@users.noreply.github.com>
2023-12-08 23:24:21 +0200

    fix

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -44,7 +44,7 @@ export default function ContactForm() {
           className={css.formInput}
           type="tel"
           name="phone" 
-          pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}" 
+          pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
           placeholder="Enter phone number" 
